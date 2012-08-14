9f31b8afa312a6d910d8ee65cfc6a1a7a086ff42
Alvaro <alvarodelvan@gmail.com>
2012-08-13 21:47:22 -0300

    ok

---
+++ entregas/algoProgAula02/src/main/java/br/com/senac/aulas/algoProgAula02/Pessoa.java
@@ -0,0 +1,18 @@
+package br.com.senac.aulas.algoProgAula02;
+
+public class Pessoa {
+	
+	private String nome = null;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	
+
+}
